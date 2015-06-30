fa61f1e8e616795a5a6da52d345d9000722ec1fe
vpatanjali <v.patanjali@gmail.com>
2015-06-30 14:05:28 +0530

    Not sure what these were

--- MS/make_libsvm.py
+++ MS/make_libsvm.py
@@ -12,11 +12,14 @@ VAL_PCT = 0.2
 
 infile = 'train_bytes_asm_tfs.csv'
 
-dev_file = 'dev_bytes_asm_tfs_10_1000.libsvm'
-val_file = 'val_bytes_asm_tfs_10_1000.libsvm'
+dev_file = 'dev_bytes_asm_ntfs_0.001_0.9.libsvm'
+val_file = 'val_bytes_asm_ntfs_0.001_0.9.libsvm'
+
+test_infiles = ['test_bytes_asm_tfs.csv', 'test_bytes_asm_tfs_part2.csv']
+
+os.chdir(DATA_DIR)
 
 #%%
-os.chdir(DATA_DIR)
 
 infile = open(infile)
 dev_file = open(dev_file,'w')
@@ -24,8 +27,10 @@ val_file = open(val_file,'w')
 
 for line in infile:
     line = line.strip().split(',')
-    line = [int(x) for x in line[1:]]
-    line = [str(line[0])] + [str(i)+':'+str(line[i]) for i in xrange(1,len(line)) if line[i] > 10 and line[i] < 1000]
+    idvs = [int(x) for x in line[2:]]
+    total = sum(idvs)*1.0
+    dv = line[1]
+    line = [dv] + [str(i)+':'+str(idvs[i]/total) for i in xrange(len(idvs)) if idvs[i]/total > 0.001 and idvs[i]/total < 0.9]
     if random.random() < VAL_PCT:
         val_file.write(' '.join(line)+'\n')
     else:
@@ -34,3 +39,18 @@ for line in infile:
 infile.close()
 dev_file.close()
 val_file.close()
+
+#%%
+
+outfile = open('test_bytes_asm_tfs.libsvm','w')
+
+for test_file in test_infiles:
+    infile = open(test_file)
+    for line in infile:
+        line = line.strip().split(',')
+        line = [line[1]] + [str(i)+':'+line[i] for i in xrange(2,len(line)) if line[i] !='0']
+        outfile.write(' '.join(line)+'\n')
+        
+    infile.close()
+
+outfile.close()
